6b3a1273e7ae85f2e37267a14e35ed7053673753
Rangarajan <rrangarajan.85@gmail.com>
2017-01-21 12:56:48 +0530

    added in January 2017 - home branch

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 cd ../git-test
-rm .fuse*
 git checkout home
 git add .
 git commit -m 'added in January 2017 - home branch'
@@ -7,6 +6,7 @@ git push git@github.com:rangaeeeee/code-bash.git
 git push origin
 git checkout master
 git pull git@github.com:rangaeeeee/code-bash.git
+rm .fuse*
 git merge home -m 'merged from home'
 git add .
 git commit -m 'added in January 2017 - master branch'
